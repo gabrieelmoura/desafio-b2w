792ef24586170d87752148cc05aae1a8da4f7233
Gabriel Moura <moura.sdp@gmail.com>
2018-06-08 23:28:03 -0300

    Inicio

---
+++ src/main/java/br/gabrieelmoura/swapi/DesafioB2WApplication.java
@@ -0,0 +1,12 @@
+package br.gabrieelmoura.swapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioB2WApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioB2WApplication.class, args);
+    }
+}
